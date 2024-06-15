e96eac2a6462e1e790c4baa9e2f8f71f4982ab22
Tomer Gozlan <tomgoz96@gmail.com>
2024-06-16 01:28:49 +0300

    batch file

--- run_video2images.bat
+++
@@ -1,2 +0,0 @@
-python run_video2Images.py  'D:\\Photogrammetry\\Photogrammetry\\DATA\\v1.mp4' 'D:\\Photogrammetry\\Photogrammetry\\OUTPUT_v1'
-python run_video2Images.py  'D:\\Photogrammetry\\Photogrammetry\\DATA\\v2.mp4' 'D:\\Photogrammetry\\Photogrammetry\\OUTPUT_v2'
